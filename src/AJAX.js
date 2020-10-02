f14de352184d30068f82971b979d01d2b11ab325
sheben <sheben404@gmail.com>
2020-10-02 17:57:07 +0800

    AJAX

---
+++ src/AJAX.js
@@ -0,0 +1,15 @@
+//简易版
+let request = XMLHttpRequest()
+request.open(GET, 'http://google.com', true)
+request.onload = ()=>{console.log(responseText)}
+request.send()
+
+//完整版
+let request = XMLHttpRequest()
+request.open(GET, 'http://google.com', true)
+request.onreadystatechange = function(){
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request.responseText)
+    }
+}
+request.send()
