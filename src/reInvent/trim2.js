76790ba83b11f3b39e597509b7108d90a75dd3f0
sheben <sheben404@gmail.com>
2020-10-16 16:09:54 +0800

    add reinvent trim

---
+++ src/reInvent/trim2.js
@@ -0,0 +1,31 @@
+function trim2(str){
+    let num = 0
+    for(let i = 0;i<str.length;i++){
+        if(str[i]===' ')
+            num++
+        else break
+    }
+    for(let j = 0;j<num;j++){
+        str = str.replace(' ','')
+    }
+    let newStr = str.split('').reverse().join('')
+    num = 0
+    for(let i = 0;i<newStr.length;i++){
+        if(newStr[i]===' ')
+            num++
+        else break
+    }
+    for(let j = 0;j<num;j++){
+        newStr = newStr.replace(' ','')
+    }
+    return newStr.split('').reverse().join('')
+}
+
+//使用正则表达式
+String.prototype.trim = function(){
+    return this.replace(/^\s+|\s+$/g, '')
+}
+//或者 
+function trim(string){
+    return string.replace(/^\s+|\s+$/g, '')
+}
