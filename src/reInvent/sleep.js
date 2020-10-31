413e92d49fc57c856cfd65a6054d71314817482d
sheben <sheben404@gmail.com>
2020-10-31 17:10:25 +0800

    update

--- src/reInvent/sleep.js
+++ src/reInvent/sleep.js
@@ -1,6 +1,6 @@
 function sleep(delay){
     let start = (new Date()).getTime()
-    while ((new Date()).getTime() - start<delay){
+    while ((new Date()).getTime() - start < delay){
         continue
     }
 }
