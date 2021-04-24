47229402f5651e3b9ab4e6dc88b333159c05fca4
sheben404 <sheben404@gmail.com>
2021-04-24 22:08:08 +0800

    update

--- src/Function/bind.js
+++ src/Function/bind.js
@@ -1,10 +1,22 @@
-Function.prototype.myBind = function (...args) {
-  let fn = this,
-    context = args[0],
-    bindArgs = args.slice(1)
+// bind 函数做了什么？
+// 对于普通函数，绑定 this 指向
+// 对于构造函数，保证原函数的原型对象上的属性不能丢失
 
-  return function (...newArgs) {
-    // 这个时候的 newArgs 是指调用 bind 返回的函数时新传入的参数
-    fn.apply(context, [...bindArgs, ...newArgs])
+Function.prototype.myBind = function (context, ...args) {
+  if (typeof this !== 'function') {
+    throw new Error('Function.prototype.bind - what is trying to be bound is not callable');
   }
+
+  // 使用 self 保存调用 bind 的函数
+  const self = this
+  const newFn = function () {}
+  const fnBound = function () {
+    self.apply(
+      context, args.concat(Array.prototype.slice().call(arguments))
+    )
+  }
+  newFn.prototype = this.prototype
+  fnBound.prototype = new newFn()
+
+  return fnBound
 }
